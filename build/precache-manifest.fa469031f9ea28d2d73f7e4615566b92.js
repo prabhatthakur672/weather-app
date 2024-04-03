0acaf444a03d985d9a1177b5136a2159cb152da2
prabhatthakur672 <142176835+prabhatthakur672@users.noreply.github.com>
2024-04-03 12:22:22 +0530

    third commit

---
+++ build/precache-manifest.fa469031f9ea28d2d73f7e4615566b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9feb7cccbe495ac0c53d76be48da01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f360bd2fc41f82591cda",
+    "url": "/static/css/main.0527dc8c.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f360bd2fc41f82591cda",
+    "url": "/static/js/main.573cafd9.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  }
+]);
